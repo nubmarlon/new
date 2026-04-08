58ab765970700ed8068e03b8170d66f1cc506827
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2026-04-08 18:31:45 +0800

    app.js

--- app.js
+++ app.js
@@ -13,13 +13,15 @@ import {
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  apiKey: "AIzaSyBvnVR4PdHp8H55VTRd6eTVQY1tiVy1Mxg",
+  authDomain: "nubmarlon-98ebe.firebaseapp.com",
+  projectId: "nubmarlon-98ebe",
+  storageBucket: "nubmarlon-98ebe.firebasestorage.app",
+  messagingSenderId: "201826490462",
+  appId: "1:201826490462:web:16c0b3c7d228e72e907ccf",
+  measurementId: "G-3WDXZ4KKXN"
 };
 
 const app = initializeApp(firebaseConfig);
